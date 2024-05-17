758a7118ec42922460fdff7e1b1df4f9a2cbe5d6
ashoollakhan <ashoollakhanii@gmail.com>
2024-05-17 15:42:12 -0300

    js finished ✅✨

--- js/game.js
+++ js/game.js
@@ -16,3 +16,48 @@ let winStates = [
     [0, 4, 8],
     [2, 4, 6],
 ]
+
+    // this function is responsible for inserting the players into the variable 
+    // board that is empty for 'o' or 'x', and making changes between the 
+    // players so we know who will play
+
+function handleMove(position) {
+    if(gameOver) {
+        return
+    }
+
+    if(board[position] == '') {
+        board[position] = symbols[playerTime]
+
+        gameOver = isWin()
+
+        if(gameOver == false) {
+            playerTime = (playerTime == 0) ? 1 : 0
+        }
+    }
+
+    return gameOver
+}
+
+    // logic to know if there is a winner.
+    // these variable positions check each of the elements in the 
+    // winSates sequence.
+    // this if checks if the positions generate a winner, if 
+    // so, it returns true
+
+function isWin() {
+    for(let i = 0; i < winStates.length; i++) {
+        let seq = winStates[i]
+        let pos1 = seq[0]
+        let pos2 = seq[1]
+        let pos3 = seq[2]
+
+        if(board[pos1] == board[pos2] &&
+           board[pos1] == board[pos3] &&
+           board[pos1] != '') {
+            return true
+        }
+    }
+
+    return false
+}
--- js/interface.js
+++ js/interface.js
@@ -7,3 +7,47 @@ document.addEventListener('DOMContentLoaded', () => {
         square.addEventListener('click', handleClick)
     })
 })
+
+    // its the function that takes the click, it will insert the players onto 
+    // the board that is empty for o or x and alternate the players 
+    // (its the handleMove function in game.js).
+    // and I also added the updateSquare function so that the squares 
+    // are updated for the symbols when we click
+
+function handleClick(event) {
+    let square = event.target
+    let position = square.id
+
+    if (handleMove(position)) {
+        setTimeout(() => {
+            alert('O jogo acabou - O vencedor foi o jogador ' + playerTime)
+        }, 10)
+    }
+    updateSquare(position)
+}
+
+// function responsible for inserting symbols into squares through html 
+// when the symbol is no longer empty, that is, when it is clicked
+
+function updateSquare(position) {
+    let square = document.getElementById(position.toString())
+    let symbol = board[position]
+    square.innerHTML = `<div class='${symbol}'></div>`
+}
+
+// function that resets the game, restoring the starting values
+
+function restartGame() {
+    board = ['', '', '', '', '', '', '', '', '',]
+    playerTime = 0
+    gameOver = false
+
+    let squares = document.querySelectorAll('.square')
+
+    squares.forEach(square => {
+        let symbol = board
+        if (symbol != '') {
+            square.innerHTML = ''
+        }
+    })
+}
